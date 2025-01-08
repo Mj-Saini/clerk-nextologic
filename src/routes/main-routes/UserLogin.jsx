f33a9129d58361a24f309b9b846094352d9e5854
Mj-Saini <119838410+Mj-Saini@users.noreply.github.com>
2025-01-08 11:14:19 +0530

    Update UserLogin.jsx

--- src/routes/main-routes/UserLogin.jsx
+++ src/routes/main-routes/UserLogin.jsx
@@ -1,42 +1,3 @@
-// import { Route, Routes } from "react-router-dom";
-// import SignInUser from "../../components/SignInUser";
-// import SignUpUser from "../../components/common/SignUpUser";
-// import VerifyEmailUser from "../../components/VerifyEmailUser";
-// import DashBoard from "../../pages/main-web/DashBoard";
-// import DashboardTable from "../../components/DashBoardTable";
-// import LandingPage from "../../pages/main-web/LandingPage";
-// import ProtectedRoute from "../../components/ProtectedRoute";
-// import FactorOne from "../../components/FectorOne";
-
-// const UserLogin = () => {
-//   return (
-//     <div>
-//       <Routes>
-//         <Route path="/" element={<LandingPage />} />
-//         <Route
-//           path="/dashboard"
-//           element={
-//             <ProtectedRoute>
-//               <DashBoard />
-//             </ProtectedRoute>
-//           }
-//         >
-//           <Route index element={<DashboardTable />} />
-//         </Route>
-//         <Route path="/sign-in" element={<SignInUser />} >
-//         <Route path="factor-one" element={<FactorOne />} />
-        
-//         </Route>
-//         <Route path="/sign-up" element={<SignUpUser />}>
-//           <Route path="verify-email-address" element={<VerifyEmailUser />} />
-//         </Route>
-//       </Routes>
-//     </div>
-//   );
-// };
-
-// export default UserLogin;
-
 
 
 
@@ -51,6 +12,7 @@ import DashBoard from "../../pages/main-web/DashBoard";
 import DashboardTable from "../../components/DashBoardTable";
 import ProtectedRoute from "../../components/ProtectedRoute";
 import FactorOne from '../../components/FectorOne';
+import AdminLogin from "../../components/AdminLogin";
 
 const UserLogin = () => {
   const navigate = useNavigate();
@@ -82,7 +44,10 @@ const UserLogin = () => {
         <Route path="/sign-up" element={<SignUpUser />}>
           <Route path="verify-email-address" element={<VerifyEmailUser />} />
         </Route>
-
+        <Route path="/admin-login" element={<AdminLogin />} />
+        <Route path="/admin-dashboard" element={<DashBoard />}>
+          <Route index element={<DashboardTable />} />
+        </Route>
         <Route
           path="*"
           element={(
