6d4cb6bfb9e21b8cb5a201446b088b5443eb4894
mj-saini <manojsaini97285@gmail.com>
2025-01-08 09:40:53 +0530

    remove islogin

--- src/routes/main-routes/UserLogin.jsx
+++ src/routes/main-routes/UserLogin.jsx
@@ -1,46 +1,4 @@
-// import { Route, Routes } from "react-router-dom";
-// import SignInUser from "../../components/SignInUser";
-// import SignUpUser from "../../components/common/SignUpUser";
-// import VerifyEmailUser from "../../components/VerifyEmailUser";
-// import DashBoard from "../../pages/main-web/DashBoard";
-// import DashboardTable from "../../components/DashBoardTable";
-// import LandingPage from "../../pages/main-web/LandingPage";
-// import ProtectedRoute from "../../components/ProtectedRoute";
-// import FactorOne from "../../components/FectorOne";
-
-// const UserLogin = () => {
-//   return (
-//     <div>
-//       <Routes>
-//         <Route path="/" element={<LandingPage />} />
-//         <Route
-//           path="/dashboard"
-//           element={
-//             <ProtectedRoute>
-//               <DashBoard />
-//             </ProtectedRoute>
-//           }
-//         >
-//           <Route index element={<DashboardTable />} />
-//         </Route>
-//         <Route path="/sign-in" element={<SignInUser />} >
-//         <Route path="factor-one" element={<FactorOne />} />
-        
-//         </Route>
-//         <Route path="/sign-up" element={<SignUpUser />}>
-//           <Route path="verify-email-address" element={<VerifyEmailUser />} />
-//         </Route>
-//       </Routes>
-//     </div>
-//   );
-// };
-
-// export default UserLogin;
-
-
-
-import { Route, Routes, useNavigate } from "react-router-dom";
-import { useUser } from "@clerk/clerk-react"; // Import the useUser hook from Clerk
+import { Route, Routes } from "react-router-dom";
 import SignInUser from "../../components/SignInUser";
 import SignUpUser from "../../components/common/SignUpUser";
 import VerifyEmailUser from "../../components/VerifyEmailUser";
@@ -48,22 +6,13 @@ import DashBoard from "../../pages/main-web/DashBoard";
 import DashboardTable from "../../components/DashBoardTable";
 import LandingPage from "../../pages/main-web/LandingPage";
 import ProtectedRoute from "../../components/ProtectedRoute";
-import FactorOne from '../../components/FectorOne';
+import FactorOne from "../../components/FectorOne";
 
 const UserLogin = () => {
-  const { isSignedIn } = useUser(); // Get the user's signed-in state from Clerk
-  const navigate = useNavigate(); // Initialize the navigate hook for redirection
-
-  if (isSignedIn) {
-    navigate("/dashboard", { replace: true });
-    return null; 
-  }
-
   return (
     <div>
       <Routes>
         <Route path="/" element={<LandingPage />} />
-        
         <Route
           path="/dashboard"
           element={
@@ -74,13 +23,10 @@ const UserLogin = () => {
         >
           <Route index element={<DashboardTable />} />
         </Route>
-
-        {/* Sign-In Route */}
-        <Route path="/sign-in" element={<SignInUser />}>
-          <Route path="factor-one" element={<FactorOne />} />
+        <Route path="/sign-in" element={<SignInUser />} >
+        <Route path="factor-one" element={<FactorOne />} />
+        
         </Route>
-
-        {/* Sign-Up Route */}
         <Route path="/sign-up" element={<SignUpUser />}>
           <Route path="verify-email-address" element={<VerifyEmailUser />} />
         </Route>
@@ -90,3 +36,6 @@ const UserLogin = () => {
 };
 
 export default UserLogin;
+
+
+
