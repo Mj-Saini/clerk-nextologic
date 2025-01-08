60e5a68e052ccffb8fe08bf7e28eaed357f5dfe9
Mj-Saini <119838410+Mj-Saini@users.noreply.github.com>
2025-01-08 11:13:09 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,10 @@
 import "./App.css";
 import UserLogin from "./routes/main-routes/UserLogin";
-import AdminDashboard from "./routes/admin-routes/AdminDashboard";
 
 function App() {
   return (
     <>
       <UserLogin />
-      <AdminDashboard />
     </>
   );
 }
